1a41c61ada841b9ce4681b4fdd67a49b209ffadd
olegall <aleynikov.oleg@gmail.com>
2021-01-12 16:11:51 +0300

    milestone

--- queries.js
+++ queries.js
@@ -7,94 +7,92 @@ const pool = new Pool({
   port: 5432
 })
 const HTTP_status = {
-	ok: 200
+	ok: 200,
+	badRequest: 400
 }
-
+const caption = {
+	invalidJSON: 'Invalid JSON'
+}
 async function getAdUnits(request, response) {
-    //console.log(request.params.data);
-    if (!request.params.data){
-  	    throw error;
-    }
-	let query = buildQuery(JSON.parse(request.params.data)); // try
+	let query = null;
+	try {
+		query = buildQuery(JSON.parse(request.params.data));
+	} catch {
+		let result = getFailedResult([caption.invalidJSON]);
+		response.status(HTTP_status.badRequest).json(result);
+	}
 	await pool.query(query, (error, results) => {
-		if (error) {
-			throw error
+		if (true) {
+			let result = getFailedResult(['error']);
+			response.status(HTTP_status.badRequest).json(result);
+			return;
 		}
 		if (request.headers.origin){
 			response.setHeader('Access-Control-Allow-Origin', request.headers.origin);
 		}
-		response.status(HTTP_status.ok).json(results.rows)
+		let result = getResult(results.rows, []);
+		response.status(HTTP_status.ok).json(result);
     })
 }
 
 async function createUpdateSite(request, response) {
-    if (!request.params.data){
-  	    throw error;
-    }
-    let data = JSON.parse(request.params.data);
+    let data = null;
+	try {
+		data = JSON.parse(request.params.data);
+	} catch {
+		let result = getFailedResult([caption.invalidJSON]);
+		response.status(HTTP_status.badRequest).json(result);
+	}
 	let createdAt = `'${data.created_at}'`;
 	let updatedAt = `'${data.updated_at}'`;
 	let domainVal = `'${data.domain}'`;
 	let query = `INSERT INTO site VALUES (${domainVal}, ${data.active}, ${createdAt}, ${updatedAt}, null)
-				 ON CONFLICT (domain) DO UPDATE SET domain = ${domainVal}, 
-													active=${data.active}, 
-													created_at = ${createdAt}, 
-													updated_at = ${updatedAt}, 
-													deleted_at = null`;
-    //console.log('***** query *****', query);
-	await pool.query(query, (error, results) => {
+				 ON CONFLICT (domain) DO UPDATE SET domain = ${domainVal}, active=${data.active}, created_at = ${createdAt}, updated_at = ${updatedAt}, deleted_at = null`;
+	await pool.query(query, async (error, results) => {
 		if (error) {
-			throw error
+			let result = getFailedResult([error]);
+			response.status(HTTP_status.badRequest).json(result);
+			return;
 		}
 		if (request.headers.origin){
 			response.setHeader('Access-Control-Allow-Origin', request.headers.origin);
 		}
-		//let result = {"success": true, }
-		response.status(HTTP_status.ok).json(results.rows)
+		let lastRow = await pool.query('SELECT id FROM site ORDER BY id DESC LIMIT 1');
+	    let result = getResult(lastRow.rows[0].id, []);
+		response.status(HTTP_status.ok).json(result);
 	})
 }
 
 async function createUpdateAdUnit(request, response) {
-	if (!request.params.data){
-		throw error;
+	let data = null;
+	try {
+		data = JSON.parse(request.params.data);
+	} catch {
+		let result = getFailedResult([caption.invalidJSON]);
+		response.status(HTTP_status.badRequest).json(result);
 	}
-	let data = JSON.parse(request.params.data);
 	let createdAt = `'${data.created_at}'`;
 	let updatedAt = `'${data.updated_at}'`;
 	let name = `'${data.name}'`;
 	let codeVal = `'${data.code}'`;
 	let params = `'${JSON.stringify(data.params)}'`;
 	let sizes = `'${JSON.stringify(data.sizes)}'`;
-	let query = `INSERT INTO ad_unit (user_id, site_id, name, code, active, params, sizes, created_at, updated_at, deleted_at) 
-									VALUES	(${data.user_id}, 
-											${data.site_id}, 
-											${name}, 
-											${codeVal}, 
-											${data.active}, 
-											${params}, 
-											${sizes}, 
-											${createdAt}, 
-											${updatedAt}, 
-											null)
-			    ON CONFLICT (code) DO UPDATE SET user_id = ${data.user_id}, 
-												site_id = ${data.site_id}, 
-												name = ${name}, 
-												code = ${codeVal}, 
-												active = ${data.active}, 
-												params = ${params}, 
-												sizes = ${sizes}, 
-												created_at = ${createdAt}, 
-												updated_at = ${updatedAt}, 
-												deleted_at = null`;
-	//console.log('***** query *****', query);
-	await pool.query(query, (error, results) => {
+	let query = `INSERT INTO ad_unit (user_id, site_id, name, code, active, params, sizes, created_at, updated_at, deleted_at)
+				VALUES (${data.user_id}, ${data.site_id}, ${name}, ${codeVal}, ${data.active}, ${params}, ${sizes}, ${createdAt}, ${updatedAt}, null)
+			    ON CONFLICT (code) DO UPDATE SET 
+				user_id = ${data.user_id}, site_id = ${data.site_id}, name = ${name}, code = ${codeVal}, active = ${data.active}, params = ${params}, sizes = ${sizes}, created_at = ${createdAt}, updated_at = ${updatedAt}, deleted_at = null`;
+	await pool.query(query, async (error, results) => {
 		if (error) {
-			throw error
+			let result = getFailedResult([error]);
+			response.status(HTTP_status.badRequest).json(result);
+			return;
 		}
 		if (request.headers.origin){
 			response.setHeader('Access-Control-Allow-Origin', request.headers.origin);
 		}
-		response.status(HTTP_status.ok).json(results.rows)
+		let lastRow = await pool.query('SELECT id FROM ad_unit ORDER BY id DESC LIMIT 1');
+		let result = getResult(lastRow.rows[0].id, []);
+		response.status(HTTP_status.ok).json(result);
 	})
 }
 
@@ -103,8 +101,8 @@ function buildQuery(params) {
 	if (params.adUnitIds){
 		for (let id = 0; id < params.adUnitIds.length; id++){
 			if (id < params.adUnitIds.length){
-				let word = query.includes('WHERE') ? 'AND' : 'WHERE';
-				query += ` ${word} id = ${id}`;
+				let word = query.includes('WHERE') ? 'OR' : 'WHERE';
+				query += ` ${word} id = ${params.adUnitIds[id]}`;
 			}
 		}
 	}
@@ -119,7 +117,13 @@ function buildQuery(params) {
 	return `${query};`;
 }
 
-//function checkParam
+function getResult(id, errors) {
+	return {"success": true, "data":{"id": id}, "errors": errors}
+};
+
+function getFailedResult(errors) {
+	return {"success": false, "data": [], "errors": errors}
+};
 
 module.exports = {
 	getAdUnits,
